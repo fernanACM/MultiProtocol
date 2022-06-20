3393667fd202b08f85d8d51225b5d979807da949
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-06-20 20:02:19 -0300

    Create Loader.php

---
+++ src/Estem0/Loader.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Estem0;
+
+use Estem0\command\MultiProtocolCommand;
+use Estem0\network\convert\MultiProtocolCraftingManager;
+use Estem0\network\convert\MultiProtocolRuntimeBlockMapping;
+use Estem0\task\CheckUpdateTask;
+use pocketmine\inventory\CraftingManager;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\ClosureTask;
+use function in_array;
+
+class Loader extends PluginBase{
+
+    /** @var string */
+    public static $resourcesPath;
+
+    /** @var self */
+    private static $instance;
+
+    /** @var MultiProtocolCraftingManager */
+    public $craftingManager;
+
+    /** @var bool */
+    public $canJoin = false;
+
+    public static function getInstance() : self{
+        return self::$instance;
+    }
+
+    public function onEnable(){
+        self::$instance = $this;
+
+        foreach($this->getResources() as $k => $v) {
+            $this->saveResource($k, $k !== "config.yml");
+        }
+
+        Config::init($this->getDataFolder() . "config.yml");
+
+        self::$resourcesPath = $this->getDataFolder();
+        MultiVersionRuntimeBlockMapping::init();
+
+        // wait until other plugin register custom craft
+        $this->getScheduler()->scheduleDelayedTask(new ClosureTask(function() : void {
+            $this->craftingManager = new MultiProtocolCraftingManager();
+            $this->canJoin = true;
+        }), 1);
+
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener(), $this);
+
+        CheckUpdateTask::init($this->getDescription()->getVersion());
+    }
+
+    public function isProtocolDisabled(int $protocol): bool{
+        $config = Config::$DISABLED_PROTOCOLS;
+        return in_array($protocol, $config, true);
+    }
+}
