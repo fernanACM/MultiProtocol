94c00cd9505dd8edd1619a24ddd775bad00bad08
Estem0 <101932306+Estem01@users.noreply.github.com>
2022-06-21 17:18:17 -0300

    Create AnimateEntityPacketTranslator.php

---
+++ src/Estem0/network/translator/AnimateEntityPacketTranslator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Estem0\network\translator;
+
+use Estem0\network\ProtocolConstants;
+use pocketmine\network\mcpe\protocol\AnimateEntityPacket;
+
+class AnimateEntityPacketTranslator{
+
+    public static function serialize(AnimateEntityPacket $packet, int $protocol) {
+        $packet->putString($packet->getAnimation());
+        $packet->putString($packet->getNextState());
+        $packet->putString($packet->getStopExpression());
+        if($protocol >= ProtocolConstants::BEDROCK_1_19_30){
+            $packet->putLInt($packet->getStopExpressionVersion());
+        }
+        $packet->putString($packet->getController());
+        $packet->putLFloat($packet->getBlendOutTime());
+        $packet->putUnsignedVarInt(count($packet->getActorRuntimeIds()));
+        foreach($packet->getActorRuntimeIds() as $id){
+            $packet->putEntityRuntimeId($id);
+        }
+    }
+}
